4e749f01b8712c54f3e32e32f0f3ab2db98560bf
LoboReflexivo <darioguerragarcia.lr@gmail.com>
2023-12-20 15:45:38 +0100

    Segundo commit

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
-const app = express(); //Con esto creamo el objeto de nuestra aplicación
+const PORT = process.env.PORT ?? 3007;
 
-const PORT = 3007;
+const app = express(); //con esto iniciamos el framework de express. todo lo que usemos con 'app.' serán funciones de Express
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.send("¡Hola, Darío!");
+  console.log("request received");
+  res.send("Hola, Darío");
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor en activo en el puerto ${PORT}`);
+  console.log(`Servidor en activo en el puerto http://localhost:${PORT}`);
 });
